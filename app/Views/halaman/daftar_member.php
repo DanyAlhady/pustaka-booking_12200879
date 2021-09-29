477d68aa2ab6caacd35250410772d103d6c0fec7
DanyAlhady <ardzafamily@gmail.com>
2021-09-29 20:50:08 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/daftar_member.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Daftar member</title>
+    </head>
+    <body>
+        <h1>Haii.. kamu mau menjadi member?</h1>
+        <p>Isikan data diri kamu dulu ya agar kami kenal dengan member kami</p>
+    </body>
+</html>
---
+++ app/Views/halaman/login.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Perpustakaan Login</title>
+    </head>
+    <body>
+        
+        <h1>Selamat Datang di Sistem Informasi Perpustakaan</h1>
+        <p>
+            Ini adalah layanan perpustakaan digital masa kini
+            <a href="<?=base_url( relativePath: '/daftar-member')?>">Klik Daftar</a> untuk mendaftar menjadi member
+        </p>
+
+    </body>
+</html>
